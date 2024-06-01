2ce821d989282372e3c22074165a9b75042df66e
TqLxQuanZ <quanpro777@hotmail.com>
2024-06-01 19:31:53 +0800

    v6.0.0 - Re-added back lostcities light, drivable block, non movable tags.

---
+++ kubejs/server_scripts/tags/drivable_blocks.js
@@ -0,0 +1,6 @@
+
+ServerEvents.tags('block', event => {
+	event.add('car:drivable_blocks', 'immersivepetroleum:asphalt')
+	event.add('car:drivable_blocks', 'immersivepetroleum:asphalt_slab')
+	event.add('car:drivable_blocks', 'immersivepetroleum:asphalt_stair')
+})
