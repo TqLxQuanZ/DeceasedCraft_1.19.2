88844305cce790d74bfd875fee6915018d0ee2df
TqLxQuanZ <quanpro777@hotmail.com>
2024-06-02 01:17:53 +0800

    v6.0.0 - Re-added back incomplete materials and custom materials using kubejs.

---
+++ kubejs/startup_scripts/items/incomplete.js
@@ -0,0 +1,67 @@
+StartupEvents.registry('item', event => {
+	event.create('incomplete_electron_tube')
+		.texture('deceasedcraft:item/incomplete_electron_tube') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	event.create('incomplete_component_electronic')
+		.texture('deceasedcraft:item/incomplete_component_electronic') 
+		.maxStackSize(64)
+		.rarity('common')
+
+	event.create('incomplete_component_electronic_adv')
+		.texture('deceasedcraft:item/incomplete_component_electronic_adv') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	// Refined Storages
+	event.create('incomplete_basic_processor')
+		.texture('deceasedcraft:item/incomplete_basic_processor') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	event.create('incomplete_improved_processor')
+		.texture('deceasedcraft:item/incomplete_improved_processor') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	event.create('incomplete_1k_storage_part')
+		.texture('deceasedcraft:item/incomplete_1k_storage_part') 
+		.maxStackSize(64)
+		.rarity('common')
+
+	event.create('incomplete_4k_storage_part')
+		.texture('deceasedcraft:item/incomplete_4k_storage_part') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	event.create('incomplete_16k_storage_part')
+		.texture('deceasedcraft:item/incomplete_16k_storage_part') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	event.create('incomplete_64k_storage_part')
+		.texture('deceasedcraft:item/incomplete_64k_storage_part') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	event.create('incomplete_64k_fluid_storage_part')
+		.texture('deceasedcraft:item/incomplete_64k_fluid_storage_part') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	event.create('incomplete_256k_fluid_storage_part')
+		.texture('deceasedcraft:item/incomplete_256k_fluid_storage_part') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	event.create('incomplete_1024k_fluid_storage_part')
+		.texture('deceasedcraft:item/incomplete_1024k_fluid_storage_part') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	event.create('incomplete_4096k_fluid_storage_part')
+		.texture('deceasedcraft:item/incomplete_4096k_fluid_storage_part') 
+		.maxStackSize(64)
+		.rarity('common')
+})
---
+++ kubejs/startup_scripts/items/materials.js
@@ -0,0 +1,68 @@
+StartupEvents.registry('item', event => {
+	event.create('rubber')
+		.texture('deceasedcraft:item/rubber') 
+		.maxStackSize(64)
+		.rarity('rare')
+		
+	event.create('polyethylene')
+		.texture('deceasedcraft:item/polyethylene') 
+		.maxStackSize(64)
+		.rarity('rare')
+
+	event.create('resistant_fabric')
+		.texture('deceasedcraft:item/resistant_fabric') 
+		.maxStackSize(64)
+		.rarity('epic')
+	
+	event.create('resistant_fiber')
+		.texture('deceasedcraft:item/resistant_fiber') 
+		.maxStackSize(64)
+		.rarity('epic')
+		
+	event.create('high_carbon_steel_alloy')
+		.texture('deceasedcraft:item/high_carbon_steel_alloy') 
+		.maxStackSize(64)
+		.rarity('epic')
+
+	event.create('silica_dust')
+		.texture('deceasedcraft:item/silica_dust') 
+		.maxStackSize(64)
+		.rarity('common')
+		
+	event.create('research_paper_1')
+		.texture('deceasedcraft:item/research_paper_1') 
+		.maxStackSize(64)
+		.rarity('rare')
+		
+	event.create('research_paper_2')
+		.texture('deceasedcraft:item/research_paper_2') 
+		.maxStackSize(64)
+		.rarity('rare')
+
+	event.create('research_paper_3')
+		.texture('deceasedcraft:item/research_paper_3') 
+		.maxStackSize(64)
+		.rarity('rare')
+
+	event.create('research_paper_4')
+		.texture('deceasedcraft:item/research_paper_4') 
+		.maxStackSize(64)
+		.rarity('rare')
+		
+	event.create('research_paper_5')
+		.texture('deceasedcraft:item/research_paper_5') 
+		.maxStackSize(64)
+		.rarity('rare')
+		
+	event.create('research_book')
+		.texture('deceasedcraft:item/research_book') 
+		.maxStackSize(1)
+		.maxDamage(4096)
+		.rarity('epic')
+		
+	event.create('formula_x')
+		.texture('deceasedcraft:item/formula_x') 
+		.maxStackSize(1)
+		.maxDamage(1024)
+		.rarity('rare')
+})
